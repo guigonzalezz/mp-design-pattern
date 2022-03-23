e5e7933209780117cc5a721c545dfc58691f189a
Guilherme Camargo Gonzalez <gcgonzalez99@hotmail.com>
2022-03-22 22:58:31 -0300

    chore: initial adjusts in the api

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('embaralhar')
+  async shufflePhrase(@Query('frase') phrase: string, @Res() response){
+    response.status(200).send( await this.appService.shufflePhrase() )
   }
 }
